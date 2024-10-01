6931339c0d87a462472be4e9920ed28357048784
mahesivaya <167259736+mahesivaya@users.noreply.github.com>
2024-10-01 16:11:03 -0400

    Delete all compiled code and ignore in future compiles

--- basic_program/target/debug/basic_program.d
+++
@@ -1,1 +0,0 @@
-/Users/maheswararaogunturi/Documents/rust_program/basic_program/target/debug/basic_program: /Users/maheswararaogunturi/Documents/rust_program/basic_program/src/main.rs
--- basic_program/target/debug/deps/basic_program-4cf6286e316740c3.d
+++
@@ -1,5 +0,0 @@
-/Users/maheswararaogunturi/Documents/rust_program/basic_program/target/debug/deps/basic_program-4cf6286e316740c3: src/main.rs
-
-/Users/maheswararaogunturi/Documents/rust_program/basic_program/target/debug/deps/basic_program-4cf6286e316740c3.d: src/main.rs
-
-src/main.rs:
